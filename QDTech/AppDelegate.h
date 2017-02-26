19c51df62c6ce2ad8d9bf01680b124d9a85d5d18
delarge77 <lastripper@hotmail.com>
2017-02-26 13:44:58 +0000

    Initial Commit

---
+++ QDTech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QDTech
+//
+//  Created by Alessandro dos santos pinto on 2/26/17.
+//  Copyright © 2017 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QDTech/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QDTech
+//
+//  Created by Alessandro dos santos pinto on 2/26/17.
+//  Copyright © 2017 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
